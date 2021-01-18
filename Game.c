a0ce083ac8e7322257565aec9876b8c5e8caf50e
Tommaso Miggia <tmiggia@gmail.com>
2021-01-18 22:00:05 +0100

    Movimento n.1

--- Game.c
+++ Game.c
@@ -1,31 +1,62 @@
 #include<stdio.h>
 
-int main(){
+/*int main(){
 	system("Color f0 && title Game");
-  int x=223,d=0,i,a=0;
-  char mov;
+  int x=223,d=1,w=1,s=1,a=1,i;
+  char movi;
   printf("\n%c\n",x);
-  menu:mov=getch();
-  mov=tolower(mov);
-  switch(mov){
+  menu:movi=getch();
+  movi=tolower(movi);
+	switch(movi){
     case 'd':
-      system("cls");
-      for(i=0;i<d;i++){
-        printf(" ");
+			system("cls");
+			for(i=0;i<s;i++){
+				printf("\n");
+			}
+			for(i=0;i<d;i++){
+				printf(" ");
       }
+			for(i=0;i<a;i++){
+				printf("\b");
+			}
       printf("%c",x);
       d++;
       goto menu;
     case 's':
-      system("cls");
-      for(i=0;i<a;i++){
+			system("cls");
+      for(i=0;i<s;i++){
         printf("\n");
       }
+			for(i=0;i<d;i++){
+				printf(" ");
+      }
+			for(i=0;i<a;i++){
+				printf("\b");
+			}
       printf("%c",x);
-      a++;
+      s++;
       goto menu;
+		case 'a':
+			system("cls");
+			for(i=0;i<a;i++){
+				printf("\n");
+			}
+			for(i=0;i<d;i++){
+				printf(" ");
+      }
+			for(i=0;i<a;i++){
+				printf("\b");
+			}
+			printf("%c",x);
+			a++;
+			goto menu;
     default:
-      system("cls");
+			printf("%c",x);
+			goto menu;
   }
 
+}*/
+
+int main(){
+	printf("carletto \b");
 }
