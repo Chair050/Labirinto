1b01fc9ced33598069228f17e404b350cfdfd0a8
Tommaso Miggia <tmiggia@gmail.com>
2021-01-29 17:34:23 +0100

    Raylib 1.0

--- Game(Raylib).c
+++ Game(Raylib).c
@@ -12,57 +12,71 @@
 #include "raylib.h"
 #include<stdlib.h>//<--Per la funzione exit
 
+int ChDraw(int Xpos, int Ypos, float Radius){
+    BeginDrawing();
+    ClearBackground(BLACK);
+    DrawCircle(Xpos,Ypos,Radius,GREEN); 
+    EndDrawing();
+}
+
 void InizioDraw(){
     BeginDrawing();
     ClearBackground(BLACK);
     DrawText("Per Uscire premi il tasto X", 90, 30, 17, GREEN);
     DrawText("Per Ingrandire lo schermo premi il tasto F",50,50,10,GREEN);
-    //Ricordarsi di mettere EndDrawing(); alla fine
+    DrawText("Premi ENTER per iniziare",80,70,17,GREEN);
+    EndDrawing();
 }
+    
 
 int main(){
-    const int AltSchermo = 800;
-    const int LargSchermo = 300;
+    const int AltSchermo = 1366;
+    const int LargSchermo = 768;
+    int Xpos = 20,Ypos = 20;
+    const float Cradius = 15;
     InitWindow(AltSchermo,LargSchermo,"The Moving - A game");
+    
     SetTargetFPS(60);
+    HideCursor();
     
     while(!WindowShouldClose()){
-    menu:InizioDraw();
-        if(IsKeyDown(KEY_W)==true){
-            InizioDraw();
-            DrawText("Stai premendo il tasto W ", 190, 200, 20, GREEN);
-            EndDrawing();
-            goto menu;
-        }
-        else if(IsKeyDown(KEY_S)==true){
-            InizioDraw();
-            DrawText("Stai premendo il tasto S ", 190, 200, 20, GREEN);
-            EndDrawing();
-            goto menu;
-        }
-        else if(IsKeyDown(KEY_A)==true){
-            InizioDraw();
-            DrawText("Stai premendo il tasto A ", 190, 200, 20, GREEN);
-            EndDrawing();
-            goto menu;
-        }
-        else if(IsKeyDown(KEY_D)==true){
-            InizioDraw();
-            DrawText("Stai premendo il tasto D", 190, 200, 20, GREEN);
-            EndDrawing();
-            goto menu;
-        }
-        else if(IsKeyDown(KEY_X)==true){
-            exit(0);
-        }
-        else if(IsKeyDown(KEY_F)==true){
-            ToggleFullscreen();
-        }
-        else{
-            InizioDraw();
-            DrawText("Non Stai premendo nessun tasto ", 190, 200, 20, GREEN);
-            EndDrawing();
-            goto menu;
+        InizioDraw();
+        if(IsKeyDown(KEY_ENTER)==true){
+          menu:if(IsKeyDown(KEY_W)==true){
+                if(Ypos>20){
+                    Ypos-=5;
+                }
+                ChDraw(Xpos,Ypos,Cradius);
+                goto menu;
+            }
+            else if(IsKeyDown(KEY_S)==true){
+                Ypos+=5;
+                ChDraw(Xpos,Ypos,Cradius);
+                goto menu;
+            }
+            else if(IsKeyDown(KEY_A)==true){
+                if(Ypos>20){
+                    Xpos-=5;
+                }
+                ChDraw(Xpos,Ypos,Cradius);
+                goto menu;
+            }
+            else if(IsKeyDown(KEY_D)==true){
+                Xpos+=5;
+                ChDraw(Xpos,Ypos,Cradius);
+                goto menu;
+            }
+            else if(IsKeyDown(KEY_X)==true){
+                exit(0);
+            }
+            else if(IsKeyDown(KEY_F)==true){
+                ToggleFullscreen();
+                goto menu;
+            }
+            else{
+                ChDraw(Xpos,Ypos,Cradius);
+                goto menu;
+            }
         }
     }
     CloseWindow();
